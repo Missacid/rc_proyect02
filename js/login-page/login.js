485e53c57053c6a7cef6b3ef93edb386216fe358
Máximo Majorel <maximomajorel@gmail.com>
2023-06-02 23:29:09 -0300

    Formulario de inicio de sesión funcional

---
+++ js/login-page/login.js
@@ -0,0 +1,93 @@
+// Usuarios predefinidos del sitio:
+
+const usuariosLocalStorage =
+  JSON.parse(localStorage.getItem("usuariosRegistrados")) || [];
+console.log(usuariosLocalStorage);
+const usuariosRegistrados = [
+  {
+    nombre: "Máximo Majorel",
+    email: "maximomajorel@gastlygames.com",
+    password: "1234",
+    active: true,
+    rol: "USER_ROLE",
+  },
+  {
+    nombre: "María Aylen Serrano",
+    email: "aylenserrano@gastlygames.com",
+    password: "1234",
+    active: true,
+    rol: "USER_ROLE",
+  },
+  {
+    nombre: "Matías Leal",
+    email: "matiasleal@gastlygames.com",
+    password: "1234",
+    active: true,
+    rol: "USER_ROLE",
+  },
+  {
+    nombre: "Santiago Gonzalez Fernandez",
+    email: "santiagogonzalez@gastlygames.com",
+    password: "1234",
+    active: true,
+    rol: "USER_ROLE",
+  },
+  {
+    nombre: "Administrador Gastly Games",
+    email: "admin@gastlygames.com",
+    password: "admin",
+    active: true,
+    rol: "ADMIN_ROLE",
+  },
+];
+if (!usuariosLocalStorage) {
+  localStorage.setItem(
+    "usuariosRegistrados",
+    JSON.stringify(usuariosRegistrados)
+  );
+}
+console.log(usuariosLocalStorage);
+
+// Elementos del DOM:
+const loginForm = document.getElementById("login-form");
+
+// Evento submit del formulario
+
+loginForm.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const emailInput = evento.target.elements.correo.value;
+  const passwordInput = evento.target.elements.contra.value;
+
+  // Verificación de usuarios
+  const usuarioEncontrado = usuariosLocalStorage.find((usuario) => {
+    if (usuario.email === emailInput) {
+      return true;
+    }
+  });
+
+  // ! Caso de usuario incorrecto o contraseña incorrecta:
+  if (!usuarioEncontrado || usuarioEncontrado.password !== passwordInput) {
+    swal({
+      title: "Error",
+      text: "El usuario o contraseña ingresados son incorrectos",
+      icon: "error",
+      button: "Intentar de nuevo",
+    });
+    return;
+  }
+  // ! Caso de usuario correcto y contraseña correcta:
+  if (usuarioEncontrado && usuarioEncontrado.password === passwordInput) {
+    delete usuarioEncontrado.password;
+    localStorage.setItem("usuarioActual", JSON.stringify(usuarioEncontrado));
+    swal({
+      title: "Bienvenido",
+      text: `Bienvenido ${usuarioEncontrado.nombre}`,
+      icon: "success",
+      button: "Continuar",
+    });
+    // ! Redirección al index.html
+    setTimeout(() => {
+      window.location.href = "/index.html"; // TODO Cambiar esto luego al panel del admin
+    }, 2000);
+  }
+});

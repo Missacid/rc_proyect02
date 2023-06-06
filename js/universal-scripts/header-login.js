2b74c89c7c25fc7b2f09c2a4c4aec6222e79cec6
Máximo Majorel <maximomajorel@gmail.com>
2023-06-06 09:23:03 -0300

    Merge desde la rama develop

---
+++ js/universal-scripts/header-login.js
@@ -0,0 +1,37 @@
+function validarSesion() {
+  const usuarioLogueado =
+    JSON.parse(localStorage.getItem("usuarioActual")) || false;
+
+  const divLogin = document.getElementById("login-container");
+
+  if (!usuarioLogueado) {
+    // Pinto boton de login si es que no hay usuario logueado
+    divLogin.innerHTML = `            <a  class="a-link-login" href="/pages/login/login.html">
+    <button class="boton-iniciar-sesion-header"><i class="fa-solid fa-user" ></i> Iniciar Sesion</button>
+  </a>`;
+  }
+  if (usuarioLogueado) {
+    divLogin.innerHTML = `<span>Hola, ${usuarioLogueado.nombre}</span> <span class="separador-botones-header">|</span> <i onclick="cerrarSesion()" id="cerrarSessionBoton" class="boton-cerrar-sesión fa-solid fa-arrow-right-from-bracket"></i>`;
+  }
+  if (usuarioLogueado.rol === "ADMIN_ROLE") {
+    const menuHeader = document.getElementById("menu-header");
+    const nuevoLi = document.createElement("li");
+    const nuevoEnlace = document.createElement("a");
+    nuevoEnlace.textContent = "Panel de Administración";
+    nuevoLi.classList.add("nav-item", "item-menu");
+    nuevoEnlace.classList.add(
+      "nav-link",
+      "active",
+      "panel-administracion-link"
+    );
+    nuevoLi.appendChild(nuevoEnlace);
+    menuHeader.appendChild(nuevoLi);
+    nuevoEnlace.href = "/index.html";
+  }
+}
+function cerrarSesion() {
+  localStorage.removeItem("usuarioActual");
+  location.reload();
+  validarSesion();
+}
+validarSesion();

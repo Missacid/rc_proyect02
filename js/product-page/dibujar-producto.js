7382091e60d0df511883d677a3515ecd9e58368a
Missacid <130413090+Missacid@users.noreply.github.com>
2023-06-11 11:56:58 -0300

    Página ADMIN
    * Página de la tienda - Maqueta Desktop
    
    * Diseño responsive en la tienda para todos los dispositivos
    
    * Agregar tabla de inventario
    
    * Implementar el inventario
    
    * Primer merge desde develop
    
    * Pequeños cambios en la plantilla entre pequeños fixes
    
    * Guardar los datos de la tabla en localStorage
    
    * Cambios en la 404 (nada importante, actualicé al header con logo)
    
    * Agregar boton destacar y guardar en el localstorage
    
    * Pequeños cambios de diseño y adicion de funcionalidades
    
    * CRUD Funcionando
    
    * Arreglado el tipo de evento del boton de subir producto
    
    * Verificacion de rol de usuario
    
    ---------
    
    Co-authored-by: Máximo Majorel <maximomajorel@gmail.com>

---
+++ js/product-page/dibujar-producto.js
@@ -0,0 +1,69 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+console.log(id);
+const productosLocal = JSON.parse(localStorage.getItem("products"));
+console.log(productosLocal);
+
+const producto = productosLocal.find((producto) => producto.codigo == id);
+console.log(producto);
+
+function dibujarPagina() {
+  const infoPrincipal = document.getElementById("product-main-info");
+  const documentTitle = document.getElementById("document-title");
+  documentTitle.innerHTML = `${producto.nombre} - Gastly Games`;
+  infoPrincipal.innerHTML = `
+        
+    <div class="product-main-info" >
+      <section class="title-container">
+        <h2 class="game-title-product-page">${producto.nombre}</h2>
+        <h3 class="game-category-product-page">${producto.categoria}</h3>
+      </section>
+      <section class="game-info-container">
+        <div class="info-game-container game-video-container">
+          <iframe width="100%" height="100%" src="${producto.link_de_video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        <div class="info-game-container game-description-container">
+          <h3>Descripción</h3>
+          <hr>
+          <p>${producto.descripcion}</p>
+        </div>
+      </section>
+      
+    </div>
+    <div class="product-main-background" style="background-image: url(${producto.banner});background-size: cover;">
+      
+      
+    </div>
+  `;
+  const compra = document.getElementById("requisitos-y-compra");
+  compra.innerHTML = `
+  <div class="product-requeriments-column requeriments-product">
+  <h3 class="requeriment-title">Requisitos </h3>
+  <hr>
+  <p>${producto.requisitos}</p>
+</div>
+<div class="product-requeriments-column purchase-column">
+  <div class="sticky-container">
+  <h3 class="purchase-title">Comprar ahora</h3>
+  <img  class="product-cover-art" src="${producto.portada}" alt="" >
+  <h3 id="precio-juego" class="product-price">$${producto.precio}</h3>
+  <div class="button-container">
+    <a href="/pages/404/404.html">
+    <button class="btn btn-danger">Comprar ahora</button>
+  </a>
+  <a href="/pages/404/404.html">
+    <button class="btn btn-secondary">Agregar al carrito</button>
+  </a>
+  </div>
+</div>
+
+</div>
+  `;
+  const precioJuego = document.getElementById("precio-juego");
+  if (producto.precio === 0) {
+    precioJuego.innerHTML = "Juego Gratuito";
+  } else {
+    precioJuego.innerHTML = `$${producto.precio}`;
+  }
+}
+dibujarPagina();
